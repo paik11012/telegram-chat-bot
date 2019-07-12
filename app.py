665ef76d45a7f9bda27cfd4a9c302ec16777e94e
paik11012 <minju11012@gmail.com>
2019-07-12 16:43:28 +0900

    Implement translation chatbot with naver papago, telegram, flask

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ API_TOKEN = config('API_TOKEN')                     #상수는 대문자
 
 app = Flask(__name__)
 
+API_TOKEN = config('API_TOKEN')
+NAVER_CLIENT_ID = config('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = config('NAVER_CLIENT_SECRET')
+
+
 @app.route('/')
 def hello():
      return 'Hello world'
@@ -24,6 +29,37 @@ def telegram():                                     #웹 북 셋팅 = 주소가 
     if from_telegram.get('message') is not None:    #from_telegram 딕셔너리 형태, get으로 접근하면 내용 없어도 오류 안뜸
         chat_id = from_telegram.get('message').get('chat').get('id')
         text = from_telegram.get('message').get('text')
+        
+        #첫 네글자가 '/번역 '일 때 확인
+        if text[0:4] == '/한영 ': 
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,#요청에 대한 정보는 headers에 있다, trailing comma
+            }
+            data = {
+                'source': 'ko',
+                'target': 'en',
+                'text': text[4:]}#4번째 글자 이후의 문자열만 대상으로 번역하겠다 알겠니?
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText')
+
+        if text[0:4] == '/영한 ':
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,#요청에 대한 정보는 headers에 있다, trailing comma
+            }
+            data = {
+                'source': 'en',
+                'target': 'ko',
+                'text': text[4:]
+            }
+            #요청보내기
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText')
+        
+        
         #send message api url
         baseurl = 'https://api.telegram.org'
         apiurl = f'{baseurl}/bot{API_TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
@@ -35,4 +71,6 @@ if __name__ == '__main__':
     app.run(debug=True)
     #ngrok 다운로드 받기 / 3/ 
     #지금까지는 get request(주소창에 뭐 치는거), post request(회원가입, 게시글 작성 등 그냥 버튼 누르는 것)
+
+    #python anywhere
     
